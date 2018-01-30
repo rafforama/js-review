4154066f89a5bc8b1d5cdf68a51cb880551d640b
rafael <rafael@rafaels-MacBook-Pro.local>
2018-01-29 19:37:37 -1000

    1st exercise complete

--- exercises.js
+++ exercises.js
@@ -9,6 +9,10 @@ Now, unleash your coding beast!
 /*1.  Variables with a String as their value.
 Declare variables named yourName, neighborName, teacherName.  Console log each variable.*/
 
+var yourName = "Rafael Montero";
+var neighborName = "Clarence Yee";
+var teacherName = "Mr. Ruppa";
+
 /*2. Variable with a Number as their value.
 Declare variables named numberOfFloorsBurjKhalifa, TokyoOlympicsYear. Please assign the actual data for each variable.
 Console log each variable.*/
